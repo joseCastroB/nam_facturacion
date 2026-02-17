b848e24d769aeefe24010cc7a6d80cd0f2915b3b
joseCastroB <josecastrobarreto10@gmail.com>
2026-02-17 17:15:15 -0500

    se agrega proyecto

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'NAM Facturación Personalizada',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Personalización de vistas de facturas de proveedor',
+    'depends': ['account', 'analytic'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ models/account_move.py
@@ -0,0 +1,44 @@
+from odoo import models, fields, api
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    # 1. NUEVO CAMPO: Fecha de Vencimiento formateada en dd/mm/aa
+    nam_fecha_vencimiento_formato = fields.Char(
+        string='Fecha de vencimiento', 
+        compute='_compute_nam_fecha_vencimiento_formato'
+    )
+
+    # 2. Campo Analítica (Lo mantenemos)
+    nam_analitica_nombres = fields.Char(
+        string='Analítica', 
+        compute='_compute_nam_analitica_nombres'
+    )
+
+    @api.depends('invoice_date_due')
+    def _compute_nam_fecha_vencimiento_formato(self):
+        for move in self:
+            if move.invoice_date_due:
+                # strftime('%d/%m/%y') fuerza el formato exacto: día/mes/año(2 dígitos)
+                move.nam_fecha_vencimiento_formato = move.invoice_date_due.strftime('%d/%m/%y')
+            else:
+                move.nam_fecha_vencimiento_formato = ""
+
+    @api.depends('invoice_line_ids.analytic_distribution')
+    def _compute_nam_analitica_nombres(self):
+        for move in self:
+            nombres = []
+            for line in move.invoice_line_ids:
+                if line.analytic_distribution:
+                    for account_id_str in line.analytic_distribution.keys():
+                        account = self.env['account.analytic.account'].browse(int(account_id_str))
+                        if account.exists() and account.name not in nombres:
+                            nombres.append(account.name)
+            
+            move.nam_analitica_nombres = ", ".join(nombres) if nombres else ""
+
+
+    # COMENTAR LAS SIGUIENTES LINEAS CUANDO SE SUBA A PRODUCCION: 
+
+    exchange_rate = fields.Float(string='Tipo de Cambio')
+    error_dialog = fields.Text(string='Mensaje de Error')
